86b1290ce647943ea727eb09bc52c22f290f16da
rwbxd <31582073+rwbxd@users.noreply.github.com>
2020-09-10 15:26:34 -0500

    Cleaning
    -Moved functions into execution order
    -Added comments
    -Fixed 'principle' typo (Should be and now is 'principal')
    -Changed 'invest' variable to slightly clearer 'invest_amount'
    -Improved the writing of some loops
    -Changed some print statements to cleaner f-strings

--- interest.py
+++ interest.py
@@ -1,35 +1,53 @@
-def variableInterest(principle, invest, take_out, interest_rate, years):
-	taken_out = int(principle * take_out)
-	if take_out != 0:
-		print("You take out $" + str(taken_out))
-	new_principle = int((principle - (taken_out) + invest) * (1 + interest_rate))
-	print("New principle is $" + str(new_principle))
-	years -= 1
-	if years > 0:
-		variableInterest(new_principle, invest, take_out, interest_rate, years)
-	else:
-		again = input("Keep new principle with different settings? (Y/N)\n")
-		if again.lower() in ("y", "yes"):
-			userPrompts(new_principle)
-		else:
-			return
+def principalPrompt():
+	principal = int(input("How much are you starting with?\n"))
+	userPrompts(principal)
 
-def principlePrompt():
-	principle = int(input("How much are you starting with?\n"))
-	userPrompts(principle)
-
-def userPrompts(principle):
-	invest = int(input("How much are you investing per year?\n"));
+def userPrompts(principal):
+	invest_amount = int(input("How much are you investing per year?\n"));
 	take_out = float(input("What percent are you taking out per year?\n"))/100;
 	interest_rate = float(input("Interest rate? (In percent)\n"))/100;
 	years = int(input("Compound for how many years?\n"));
-	variableInterest(principle, invest, take_out, interest_rate, years)
+	variableInterest(principal, invest_amount, take_out, interest_rate, years)
 
-principlePrompt();
+def variableInterest(principal, invest_amount, take_out, interest_rate, years):
+	taken_out = int(principal * take_out) #Calculate amount to take out
+	if take_out != 0:
+		principal -= taken_out #Take out calculated amount
+		print(f"You take out ${taken_out}")
 
-retry = input("Press ENTER to restart, type exit to close the program\n");
+	#Interest forumla A = P(1+r)^1, where A is value after interest,
+	#P is the principal, r is the interest rate.
+	#
+	#I could use the formula A = P(1+r)^Y, where Y is the years compounded,
+	#but I want to see the principal for each year.
+	new_principal = int((principal + invest_amount) * (1 + interest_rate))
+	print(f"New principal is ${new_principal}")
 
-if retry == "exit":
-	exit()
-else:
-	principlePrompt()
+	#Check if there's more interest to be calculated
+	years -= 1
+	if years > 0:
+		variableInterest(new_principal, invest_amount, take_out, interest_rate, years)
+		#This breaks the PEP-8 standard, but it's the only line that does so
+		#Therefore it's staying as is for now
+		#The standard with line breaks looks ugly in my opinion and this context
+	else:
+		while True:
+			again = input("Keep new principal with different settings? (Y/N)\n")
+			again = again.lower() #New line to conform to PEP-8 80 char limit
+			if again in ["y", "yes"]: #Only way I know to validate input, haha
+				userPrompts(new_principal)
+				break
+			elif again in ["n", "no"]:
+				retry()
+				break
+			else:
+				print("Invalid input!")
+
+def retry():
+	retry = input("Press ENTER to restart, type exit to close the program\n");
+	if retry.lower() == "exit":
+		exit()
+	else:
+		principalPrompt()
+
+principalPrompt();
